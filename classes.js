e1dd23b6dea6d973c117b0d908c436ee01455a17
KJ Moses <kjm7832@nyu.edu>
2026-03-23 12:09:29 -0400

    created gemstone class and completed scene 1

--- classes.js
+++ classes.js
@@ -1,5 +1,34 @@
+let colors = ['#C00A02','#9D47CE', '#85C3F4', '#DDE1E2', '#19771F','#DCD5DC','#95142A', '#B1C748', '#1403B3', '#95B9AF', '#AC5208', '#1FA0B5'];
+let sizes = [50, 100, 150, 200, 250, 300];
 class Gemstone{
-  constructor(){}
+  constructor(x, y){
+    this.x = x; 
+    this.y = y; 
+    this.color = random(colors); 
+    this.size = random(sizes); 
+    // this.shine = shine; 
+  }
+  display(){
+    // hexagon starter code from p5js - https://editor.p5js.org/jenagosta/sketches/Sy5wzBblg
+    stroke(this.color); 
+    strokeWeight(2);
+    fill(this.color, 240);
+
+    push(); 
+    translate(this.x, this.y);
+    scale(this.size/100);
+
+    beginShape();
+      vertex(-75, -130); 
+      vertex(75, -130); 
+      vertex(150, 0); 
+      vertex(75, 130); 
+      vertex(-75, 130); 
+      vertex(-150, 0);
+    endShape(CLOSE); 
+    
+    pop();
+  }
 }
 
 class Star{
--- sketch.js
+++ sketch.js
@@ -13,34 +13,117 @@ let mirror = 2;
 let forge = 3; 
 let stars = 4; 
 
-
 //Starting state
 let scene = brush; 
 
+//scene variables 
+//scene1 - toothbrush
+let cleanLevel = 120; 
+let clean = false; 
+
+//scene2 = 
+
+let skies = ['#C1E8FF', '#7DA0CA', "#5483B3", '#052659', '#021024']
 function setup() {
  // put setup code here
  createCanvas(800,800);
- background(196,216,255);
+ rectMode(CENTER);
 }
 
 function draw() {
   // put drawing code here
-  background(196,216,255);
-  sunrise();
-}
+  if (scene == brush){
+    scene1(); 
+  }else if (scene == gems){
+    scene2();
+  } else if (scene == mirror){
+    scene3()
+  } else if (scene == forge){
+    scene4();
+  } else if (scene == stars){
+    scene5();
+  } else { scene == brush}
 
-//State 1 - sun rises up to middle of screen, turns into a face, shows teeth and users has to brush teeth to white, once complete 
-function sunrise(){
-    fill(255,206,27);
-    noStroke();
-    ellipse(mouseX, mouseY, 120,120);
-    ellipse(mouseX + 40, mouseY + 40, 150, 150);
 }
-
-function scene_change(){
-  if (scene > stars) {
-    emotion = brush;
-  } else {
-    scene++;
+ 
+function scene1(){
+  // change the scene
+  background(skies[brush]);
+  //face
+  noStroke();
+  fill(247,200,92);
+  ellipse(width/2, height/2, 500,500); 
+  //teeth
+  strokeWeight(2);
+  fill(cleanLevel); 
+  arc(width/2, height/2 + 50, 350, 300, 0, PI);
+  let closeToTeeth = dist(mouseX, mouseY, width/2, height/2 +50); 
+  if (closeToTeeth < 120){
+    cleanLevel++; 
+    cleanLevel = constrain(cleanLevel, 0, 255); 
   }
+  if (cleanLevel >= 255){
+    clean = true; 
+    for (let i = 0; i < 5; i++){
+    sparkle(random(width/2 - 100, width/2 + 100), random(height/2, height/2 + 80),random(40, 50));
+  }
+  }
+  //toothBrush
+  noStroke();
+  fill(44,104,123);
+  rect(mouseX, mouseY, 80, 80);
+  rect(mouseX + 40, mouseY, 160,50);
+
 }
+//helper function
+function sparkle(x, y, size){
+  // four pointed sparkle coords 
+  // from up-right-down--left = (0,-50), (5,-5), (50,0), (5,5), (0,50), (-5,5)(-50,0)(-5,-5)(0,-50)
+  push(); 
+  translate(x,y);
+  stroke(180);
+  strokeWeight(2);
+  fill(255, random(200, 255));
+
+  beginShape();
+    vertex(0, -size); // start at top
+    bezierVertex(size*0.05, -size*0.3, size*0.7, -size*0.1, size, 0); // right
+    bezierVertex(size*0.7, size*0.1, size*0.05, size*0.3, 0, size);    // bottom
+    bezierVertex(-size*0.05, size*0.3, -size*0.7, size*0.1, -size, 0); // left
+    bezierVertex(-size*0.7, -size*0.1, -size*0.05, -size*0.3, 0, -size);// top
+  endShape(CLOSE);
+}
+// scene 2
+function scene2(){
+  background(skies[gems]);
+}
+function scene3(){
+  background(skies[mirror]);
+}
+function scene4(){
+  background(skies[forge]);
+}
+function scene5(){
+  background(skies[stars]);
+}
+
+
+//test for  change in skies 
+function mousePressed(){
+scene = (scene + 1) % 5;
+}
+
+
+
+
+
+
+
+// doesnt work rn idk why 
+// function scene_change(){
+//   if (scene > stars) {
+//     emotion = brush;
+//   } else {
+//     scene++;
+//   }
+// }
